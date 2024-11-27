3b6c84d4640a3572e20c0d667bb00beba85695c1
brk2ran <turanburak@gmx.de>
2024-11-27 16:43:21 +0100

    frontend von neu

--- frontend/src/api/categoryService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = 'http://localhost:5000/api/categories';
-
-export const fetchCategoriesWithCounts = async () => {
-    const response = await axios.get(`${API_BASE}/with-counts`);
-    return response.data;
-};
--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p>&copy; 2024 Magische Items</p>
-        </footer>
-    );
-}
-
-export default Footer;
